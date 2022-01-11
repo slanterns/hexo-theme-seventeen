ce814a2ad73fbbe5ed307b94e137d4ca9aa165c9
slanterns <slanterns.w@gmail.com>
2022-01-12 02:24:32 +0800

    url_for -> full_url_for
    `url_for` cannot get desired path when website is in subdirectory,
    use `full_url_for` instead.

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -2,13 +2,13 @@
 
 // var version = ("0000" + (Math.random()*Math.pow(36,4) << 0).toString(36)).slice(-4);
 
-var url_for = function (path) {
-    return hexo.extend.helper.store['url_for'].call(hexo, path)
+var full_url_for = function (path) {
+    return hexo.extend.helper.store['full_url_for'].call(hexo, path)
 }
 
 hexo.extend.helper.register('asset', (path) => {
     // return url_for(path) + '?v=' + version;
-    return url_for(path);
+    return full_url_for(path);
 });
 
 // For Chrome
